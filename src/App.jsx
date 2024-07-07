c0fd074de9b2e46b14b32974ab521959711bb0c1
ernesto2011 <skillmx107@gmail.com>
2024-07-06 22:00:53 -0600

    add: reducers

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './store/slices/counter'
+
+function App() {
+  const {counter} = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>count is {counter}</h1>
+      <div className="card">
+        <button onClick={() =>dispatch(increment())}>
+          Increment
+        </button>
+        <button onClick={() =>dispatch(decrement())}>
+          Decrement
+        </button>
+        <button onClick={() =>dispatch(incrementByAmount(2))}>
+          Increment by 2
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
